6bed4edb785b07d748141d9fb77518ca0a1832df
Ayomikun Oyeniyi <ayomikunoyeniyi07@gmail.com>
2023-10-22 21:14:18 -0400

    Add equals button functionality

--- script.js
+++ script.js
@@ -1,37 +1,96 @@
-function add(x,y) {
-   console.log(x + y);
-};
-function subtract(x,y) {
-    console.log(x - y);
-};
-function divide(x,y) {
-    console.log(x / y);
-};
-function multiply (x,y) {
-    console.log(x * y);
+//functions for basic math operations
+let result = '';
+
+//declaring variables for numbers and operators
+let firstNumber = '';
+let operator = '';
+let secondNumber = '';
+
+
+function add(firstNumber, secondNumber) {
+    result = firstNumber + secondNumber;
+    display.textContent = result;
 };
 
-let firstNumber;
-let operator;
-let secondNumber;
-
-function operate (firstNumber, operator, secondNumber) {
-    operator(firstNumber, secondNumber);
+function subtract(firstNumber, secondNumber) {
+    result = firstNumber + secondNumber;
+    display.textContent = result;
 };
 
-let buttonNumber;
+function multiply(firstNumber, secondNumber) {
+    result = firstNumber * secondNumber;
+    display.textContent = result;
+};
 
-const buttons = document.querySelectorAll(".number");
-buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        buttonNumber = button.innerHTML;
-        const display = document.querySelector(".display");
-        display.textContent += buttonNumber;
-    })
+function divide(firstNumber, secondNumber) {
+    result = firstNumber / secondNumber;
+    display.textContent = result;
+};
+
+
+
+
+
+
+//operate function
+function operate() {
+    firstNumber = Number(firstNumber);
+    secondNumber = Number(secondNumber);
+
+    if (operator === '+') {
+        add(firstNumber, secondNumber);
+    } else if (operator === '-') {
+        subtract(firstNumber, secondNumber);
+    } else if(operator === 'x') {
+        multiply(firstNumber, secondNumber);
+    } else if (operator === '/') {
+        if (secondNumber === 0) {
+            display.textContent = "Error"
+        }
+        divide(firstNumber, secondNumber);
+    };
+};
+
+
+// clear button functionality
+const clearButton =  document.querySelector(".clear");
+
+clearButton.addEventListener('click', () => {
+    display.textContent = ' ';
+})
+
+//functions to display numbers
+//first, to call the display
+const display = document.querySelector(".display");
+
+//then for the number buttons
+const numberButtons = document.querySelectorAll(".number");
+numberButtons.forEach((numberButton) => {
+    
+    numberButton.addEventListener('click', () => {
+        if (operator === '') {
+            firstNumber += numberButton.textContent;
+            display.textContent = firstNumber;
+        } else {
+            secondNumber += numberButton.textContent;
+            display.textContent = secondNumber;
+        };
+        
+    });
+
 });
 
-const addButton = document.querySelector(".addition");
-addButton.addEventListener('click', add);
 
-const subtractButton = document.querySelector(".subtraction");
-subtractButton.addEventListener('click', subtract);
+//then for the operator buttons
+const operatorButtons = document.querySelectorAll(".operator");
+operatorButtons.forEach((operatorButton) => {
+
+    operatorButton.addEventListener('click', () => {
+        operator = operatorButton.textContent;
+        display.textContent += " " + operator;
+    });
+});
+
+// for the equals button
+const equalsButton = document.querySelector(".equals");
+equalsButton.addEventListener('click', operate);
